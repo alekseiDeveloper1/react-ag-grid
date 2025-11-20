3715dda328c39f48908fbb6119b061f1ddf7b577
Aleksey <Aleks_266@mail.ru>
2025-11-20 17:59:31 +0300

    fix: the article display

--- src/features/stats/stats-grid/stats-grid.tsx
+++ src/features/stats/stats-grid/stats-grid.tsx
@@ -2,7 +2,7 @@ import { AgGridReact } from 'ag-grid-react';
 import { useEffect, useState } from 'react';
 import { IStatItem } from '../../../types/stats.types';
 import { STATS_API } from '../../../api/stats.api';
-import { ColDef, themeBalham } from 'ag-grid-enterprise';
+import { ColDef, ICellRendererParams, themeBalham } from 'ag-grid-enterprise';
 import { useSearchParams } from 'react-router-dom';
 import { Metrics } from '../stats.const';
 import { statsGridColumnsFactory } from './stats-grid.columns';
@@ -20,16 +20,26 @@ export function StatsGrid() {
     }, [metric]);
 
     useEffect(() => {
-        STATS_API.getFull().then((data) => setRowData(data));
+        STATS_API.getShort().then((data) => setRowData(data));
     }, []);
+
+    const showArticleInGroup = {
+        innerRenderer: (params: ICellRendererParams) => {
+            if (!params.data) {
+                return params.value || '';
+            }
+            return params.data.article;
+        },
+    }
 
     return (
         <div className='stats-grid ag-theme-balham'>
             <AgGridReact
-                groupHideParentOfSingleChild='leafGroupsOnly'
                 autoGroupColumnDef={{
                     menuTabs: ['columnsMenuTab'],
                     pinned: 'left',
+                    cellRenderer: 'agGroupCellRenderer',
+                    cellRendererParams: showArticleInGroup,
                 }}
                 theme={themeBalham.withParams({
                     backgroundColor: 'var(--bs-body-bg)',
